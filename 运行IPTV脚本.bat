82aa7c288ac241179f8bd9a0f2193ad12fc7c460
yecaifa <1499598221@qq.com>
2026-01-02 00:31:43 +0800

    Update repo files (有效组播第1新IP) - 2026-01-02 00:31:42

--- 运行IPTV脚本.bat
+++ 运行IPTV脚本.bat
@@ -1,14 +1,9 @@
 @echo off
-chcp 65001 >nul
-
+chcp 65001 > nul
 echo ==============================================
-echo        通过 PowerShell 运行 IPTV 脚本
+echo          通过PowerShell运行IPTV脚本
 echo ==============================================
 echo.
+powershell -ExecutionPolicy Bypass -Command "& python 'iptv_m3u_get.py'; pause"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "cd /d '%~dp0'; python iptv_m3u_get.py"
-
-echo.
-echo 运行完成，按任意键退出...
-pause >nul
+pause > nul
